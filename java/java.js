72fb16f83b4b6b5c3ba0b0c81626d617be2fdfd9
Simon <uniond44@gmail.com>
2023-08-19 00:24:15 +0200

    Main Page update

---
+++ java/java.js
@@ -0,0 +1,48 @@
+emne1()
+function underConstruction() {
+    alert("Under Construction.");
+}
+
+function emne1() {
+    document.getElementById("emne3").innerHTML = ``;
+    document.getElementById("emne2").innerHTML = ``;
+    document.getElementById("emne1").innerHTML = `
+        
+        <a href="Emne1/Eksempel1/index.html"><button class="emnebuttons">Eksempel 1</button></a>
+        <a href="Emne1/ukeoppgave1/index.html"><button class="emnebuttons">Uke oppgave 1</button></a>
+        <a href="Emne1/Visuell_teller/index.html"><button class="emnebuttons">Visuell teller</button></a>
+        <a href="Emne1/Stoppeklokke/index.html"><button class="emnebuttons">Stoppeklokke</button></a>
+        <a href="Emne1/Adjektivhistorie/index.html"><button class="emnebuttons">Adjectiv Historie</button>
+        <a href="Emne1/Trafikklys/index.html"><button class="emnebuttons">Trafikklys</button></a>
+        <a href="Emne1/SoundBoard/index.html"><button class="emnebuttons">Sound Board</button></a>
+        <a href="Emne1/CrockGame/index.html"><button class="emnebuttons">Crock Game</button></a>
+        <p>-- Unfinished Projects --</p>
+        <a href="Emne1/"><button class="emnebuttons">Lyspære</button></a>
+        <a href="Emne1/"><button class="emnebuttons">Super Mario</button></a>
+        <a href="Emne1/"><button class="emnebuttons">Endre Dukken</button></a>
+        <a href="Emne1/"><button class="emnebuttons">Chatbot</button></a>
+        <a href="Emne1/"><button class="emnebuttons">Guess the Number</button></a>
+        <a href="Emne1/"><button class="emnebuttons">Stein, Saks, Papir</button></a>
+        <a href="Emne1/"><button class="emnebuttons">Orakelet</button></a>
+        <a href="Emne1/"><button class="emnebuttons">Handleliste</button></a>
+        <a href="Emne1/"><button class="emnebuttons">Pokemon Catcher</button></a>
+        <a href="Emne1/"><button class="emnebuttons">Morsekode Oversetter</button></a>
+        <a href="Emne1/"><button class="emnebuttons">Magic 8 Ball</button></a>
+        <a href="Emne1/"><button class="emnebuttons">Simon Says</button></a>
+        <a href="Emne1/"><button class="emnebuttons">Pokemon</button></a>
+        <a href="Emne1/"><button class="emnebuttons">Tampen Brenner</button></a>
+        <a href="Emne1/"><button class="emnebuttons">Det Norske Flagg</button></a>
+        <a href="Emne1/"><button class="emnebuttons">Sjakkbrett</button></a>
+        <a href="Emne1/"><button class="emnebuttons">Debug This</button></a>
+    `;
+}
+function emne2() {
+    document.getElementById("emne3").innerHTML = ``;
+    document.getElementById("emne2").innerHTML = ``;
+    document.getElementById("emne1").innerHTML = ``;
+}
+function emne3() {
+    document.getElementById("emne3").innerHTML = ``;
+    document.getElementById("emne2").innerHTML = ``;
+    document.getElementById("emne1").innerHTML = ``;
+}
